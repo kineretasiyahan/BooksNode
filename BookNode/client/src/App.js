2b0a8c72e2b77209ab140efbf2303f07d52b52b5
SHUANESH <shuanesh1817@gmail.com>
2021-11-25 17:52:22 +0200

    add-home-style

--- BookNode/client/src/App.js
+++ BookNode/client/src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-<<<<<<< HEAD
 import AppRouter from './components/routing/AppRouter';
-=======
-import AppRouter from './components/routing/AppRouter'
 import NavBar from './components/Features/navBar/NavBar'
->>>>>>> cb7e00f7d19f2a40b5f40ae21f535d60b229acd6
-
 
 function App() {
   return (
       <Router>
+        
         <AppRouter/>
       </Router>
   )
--- BookNode/client/src/components/Pages/Home/Home.jsx
+++ BookNode/client/src/components/Pages/Home/Home.jsx
@@ -1,50 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { simulation } from "../../simulation";
-import { ImCart } from "react-icons/im";
+import { ImCart , ImBook} from "react-icons/im";
 import { getAllBooks } from "../../../service/books";
-
+import Image from "../../Features/image/Image";
+import "./home.scss";
 function Home() {
   const [books, setBook] = useState([]);
+  const [heightImg, setHeightImg] = useState(800);
 
   useEffect(() => {
     getAllBooks()
-      .then((res) =>{ setBook(res)})
+      .then((res) => {
+        setBook(res);
+      })
       .catch((e) => {
         console.log("ERROR!! " + e);
-      });
+      })
+    //  console.log(heightImg);
+    //  calcImageSize()
   }, []);
 
-  
+  // const calcImageSize = () => {
+  //   const targetSize = 800;
+  //   const imagesPerRow =Math.floor(Math.round(heightImg / targetSize)) ;
+  //   const size = heightImg / imagesPerRow  ;
+  //   console.log(size);
+  //   setHeightImg(size)
+  // }
+
+
+
   return (
-    <div>
-      <h2>The most popular books...</h2>
+    <div className="home-root">
+
+      <div className="home-intro animate__animated animate__zoomIn"  style={{backgroundImage:`url(https://images.pexels.com/photos/1165982/pexels-photo-1165982.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=${heightImg}&w=${heightImg})`}}>
+        <h1> Booknode <br/> A place where imagination has no boundaries ...</h1>
+      </div>
+      
+      <div  className="home-images">
       {books?.map((book, index) => {
         if (index < 4) {
           return (
-            <div key={index}>
-              <img src={book.pic} />
-              <h3>{book.name}</h3>
-              <h5>{book.author}</h5>
-              <p>{book.summary}</p>
-              <button>
-                <i>Add to {<ImCart />}</i>
-              </button>
-            </div>
+            <Image 
+              key={index}
+              nameBook={book.name}
+              image={book.pic}
+              author={book.author}
+              summary={book.summary?.slice(0,80) + "..."}
+            />
           );
         }
-      })}
+      })}        
+      </div>
+
     </div>
   );
 }
 
 export default Home;
-
-// import React from 'react'
-
-// const Home =()=> {
-//     return (
-//         <div>
-//          <h1> Hello world</h1>
-//         </div>
-//     )
-// }
