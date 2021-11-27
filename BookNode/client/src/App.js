94a687a132e614c9ebc1dc5082c4f51628ddd5c1
HanaAmara <hhanaamara@gmail.com>
2021-11-27 22:00:07 +0200

    fetch

--- BookNode/client/src/App.js
+++ BookNode/client/src/App.js
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import AppRouter from './Components/routing/AppRouter'
-
-
 // import NavBar from './components/Features/navBar/NavBar'
 
 
--- BookNode/client/src/components/Pages/Home/Home.jsx
+++ BookNode/client/src/components/Pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { ImCart } from "react-icons/im";
 import { getAllBooks } from "../../../service/books";
-import Button from "../../Features/button/Button";
+// import Button from "../../Features/button/Button";
 import Input from "../../Features/input/Input";
 const Home = () => {
   const [books, setBook] = useState([]);
   const [input, setInput] = useState("");
-  const [booksbbb, setBookbbb] = useState([books]);
+  const [booksbbb, setBookbbb] = useState(books);
 
   useEffect(() => {
     getAllBooks()
