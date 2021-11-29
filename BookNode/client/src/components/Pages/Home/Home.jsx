f43aa3bb435c4a1c43857676a1cd46ce10ad67cf
kineretasiyahan <kineret9688@gmail.com>
2021-11-29 15:32:01 +0200

    new commit

--- BookNode/client/src/components/Pages/Home/Home.jsx
+++ BookNode/client/src/components/Pages/Home/Home.jsx
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { ImCart } from "react-icons/im";
 import { getAllBooks } from "../../../service/books";
-<<<<<<< HEAD
-import Input from "../../Features/input/Input";
-import Image from "../../Features/image/Image";
-import "./home.scss";
-=======
 import Button from "../../Features/button/Button";
 // import Button from "../../Features/button/Button";
->>>>>>> 1eede7bf4bfbcdd9a9fd5264a52c9764a233a47e
 
 import Input from "../../Features/input/Input";
 const Home = () => {
   const [books, setBook] = useState([]);
   const [input, setInput] = useState("");
-<<<<<<< HEAD
-  const [searchBookResult, setSearchBookResult] = useState(books);
-=======
 
   const [booksbbb, setBookbbb] = useState([books]);
 
   const [searchBookResult, setSearchBookResult] = useState([books]);
->>>>>>> 1eede7bf4bfbcdd9a9fd5264a52c9764a233a47e
   const [heightImg, setHeightImg] = useState(800);
 
 
@@ -44,33 +34,9 @@ const Home = () => {
     console.log(searchResult);
     setBookbbb(searchResult);
   };
-<<<<<<< HEAD
-
-  // const calcImageSize = () => {
-  //   const targetSize = 800;
-  //   const imagesPerRow =Math.floor(Math.round(heightImg / targetSize)) ;
-  //   const size = heightImg / imagesPerRow  ;
-  //   console.log(size);
-  //   setHeightImg(size)
-  // }
-
-  useEffect(() => {
-    getAllBooks()
-      .then((res) => setBook(res.data))
-      .catch((e) => {
-        console.error(e);
-      });
-    //  console.log(books);
-  }, []);
-
-  let currentBooks;
-  if (input.length > 0) {
-    currentBooks = searchBookResult;
-=======
   let mathArray;
   if (input.length >0) {
     mathArray = booksbbb;
->>>>>>> 1eede7bf4bfbcdd9a9fd5264a52c9764a233a47e
   } else {
     mathArray = books;
   }
