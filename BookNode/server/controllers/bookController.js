7fb43d8b5328b41476c0f2208de70fd24d0fc4e2
kineretasiyahan <kineret9688@gmail.com>
2022-01-03 00:31:51 +0200

    new changes

--- BookNode/server/controllers/bookController.js
+++ BookNode/server/controllers/bookController.js
@@ -4,35 +4,20 @@ const getAllBooks = async (req, res) => {
   try {
     const books = await bookModel.find({});
     if (!books) throw error;
-    res.status(200).json({
-      success: true,
-      message: "success",
-      data: result,
-    });
+    res.status(200).json({ data: books });
   } catch (err) {
-    res.status(301).json({
-      message: "error in method get books",
-      success: false,
-      error: err.message
-    })
+    res.status(301).json(err.message + " error in method get books");
   }
 };
+
 const getBookById = async (req, res) => {
   try {
     bookModel.findById(req.params.id, (error, result) => {
       if (error) throw error;
-      res.status(200).json({
-        success: true,
-        message: "success",
-        data: result,
-      });
+      res.status(200).json({ data: result });
     });
   } catch (err) {
-    res.status(301).json({
-      message: "error in method get books by id",
-      success: false,
-      error: err.message
-    })
+    res.status(301).json("error in method get books");
   }
 };
 
@@ -40,18 +25,10 @@ const updateBookById = async (req, res) => {
   try {
     bookModel.findOneAndUpdate(req.params.id, (error, result) => {
       if (error) throw error;
-      res.status(200).json({
-        success: true,
-        message: "success",
-        data: result,
-      });
+      res.status(200).json({ data: result });
     });
   } catch (err) {
-    res.status(301).json({
-      message: "error in method update books",
-      success: false,
-      error: err.message
-    })
+    res.status(301).json("error in method get books");
   }
 };
 
@@ -59,38 +36,28 @@ const deleteBookById = async (req, res) => {
   try {
     bookModel.findByIdAndDelete(req.params.id, (error, result) => {
       if (error) throw error;
-      res.status(200).json({
+            res.status(200).json({
         success: true,
         message: "success",
         data: result,
       });
     });
   } catch (err) {
-    res.status(301).json({
-      message: "error in method delete books",
-      success: false,
-      error: err.message
-    })
+    res.status(301).json("error in method get books");
   }
 };
+
 const createBook = async (req, res) => {
   try {
     await bookModel.insertMany(req.body.books, (error, result) => {
       if (error) throw error;
-      res.status(200).json({
-        success: true,
-        message: "success",
-        data: result,
-      });
+      return res.status(200).json({ data: result });
     });
   } catch (err) {
-    res.status(301).json({
-      message: "error in method post books",
-      success: false,
-      error: err.message
-    })
+    res.status(301).json("error in method get books");
   }
 };
+
 module.exports = {
   getAllBooks,
   createBook,
